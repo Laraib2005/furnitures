d3da154021f1b7c3a5ccd79a20c34c2b2833f98f
Laraib Rizwan <161979255+Laraib2005@users.noreply.github.com>
2025-02-05 06:20:56 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -43,17 +43,17 @@ const Contact = () => {
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 ADDRESS
               </h2>
-              <p className="mt-1">Naveed Arcade Saddar Karachi</p>
+              <p className="mt-1"> Karachi</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-500">ms5901562@gmail.com</a>
+              <a className="text-indigo-500">laraibrizwan221@gmail.com</a>
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p>03420032995</p>
+              <p>0365256477</p>
             </div>
           </div>
         </div>
